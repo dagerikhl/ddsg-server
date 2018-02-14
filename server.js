4830211c0e32e1559aa820586d8257b8a4c4ed45
Dag Erik <dagerikhl@gmail.com>
2018-02-14 11:46:03 +0100

    Add config file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const schedule = require('node-schedule');
+const config = require('./config');
 
 // Update entities on a schedule, CRON syntax: '0 0 * * *' = once a day at 00:00
 schedule.scheduleJob('0 0 * * *', updateEntitiesFromDataSources);
@@ -7,13 +8,10 @@ schedule.scheduleJob('0 0 * * *', updateEntitiesFromDataSources);
 // Register Express app
 const app = express();
 
-// Configuration
-const port = 8000;
-
 // Register routes
 require('./app/routes')(app);
 
 // Listen on port
-app.listen(port, () => {
-    console.log('Server started on port ' + port + '. Listening...');
+app.listen(config.server.port, () => {
+    console.log('Server started on port ' + config.server.port + '. Listening...');
 });
