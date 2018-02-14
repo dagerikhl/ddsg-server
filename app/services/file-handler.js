322cc16ce48620badf07a1e90c1c391e7e6a00d3
Dag Erik <dagerikhl@gmail.com>
2018-02-14 13:21:27 +0100

    Add missing file extension to fileHandler

--- app/services/file-handler.js
+++ app/services/file-handler.js
@@ -4,15 +4,14 @@ const path = require('path');
 const basePath = 'data';
 
 function getFileContent(filename) {
-    const filePath = path.join(basePath, filename);
+    const filePath = path.join(basePath, filename + '.json');
     const rawContent = fs.readFileSync(filePath);
 
     return JSON.parse(rawContent.toString());
 }
 
-// TODO Replace entire content of local file
 function setFileContent(filename, content) {
-    const filePath = path.join(basePath, filename);
+    const filePath = path.join(basePath, filename + '.json');
     const rawContent = JSON.stringify(content);
 
     fs.writeFileSync(filePath, rawContent);
