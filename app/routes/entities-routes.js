c4012a231a7eb05d168f64fdbbeca1e1eece62df
Dag Erik <dagerikhl@gmail.com>
2018-02-14 13:52:29 +0100

    Add log statement to API endpoint

--- app/routes/entities-routes.js
+++ app/routes/entities-routes.js
@@ -2,6 +2,8 @@ const fileHandler = require('../services/file-handler');
 
 module.exports = (app) => {
     app.get('/entities', (req, res) => {
+        console.log(`Serving: ${req.method} ${req.headers.host || ''}${req.url}`);
+
         const content = fileHandler.getFileContent('entities');
         res.json(content);
     });
