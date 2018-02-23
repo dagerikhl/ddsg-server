a22ce2131a88a2690614bfe2de86b498e821b50e
Dag Erik <dagerikhl@gmail.com>
2018-02-23 12:34:16 +0100

    Improve description parsing for capec

---
+++ app/services/stix/attack-pattern-generator.js
@@ -0,0 +1,125 @@
+const utilities = require('../utilities');
+
+const stixGeneral = require('./general-generator');
+
+let capecObject = null;
+
+function feed(feedObject) {
+    capecObject = feedObject;
+}
+
+function clear() {
+    capecObject = null;
+}
+
+function createEntity(type) {
+    const uuid = utilities.uuid();
+    const timestamp = utilities.timestamp();
+
+    return {
+        type: `${type}`,
+        id: `${type}--${uuid}`,
+        created: timestamp,
+        modified: timestamp
+    };
+}
+
+function genName() {
+    return capecObject['_attributes']['Name'];
+}
+
+function genDescription() {
+    return stixGeneral.buildRecursiveCapecDescription([], capecObject['capec:Description']);
+}
+
+function genExternalReferences() {
+    return [{
+        source_name: 'capec',
+        id: `CAPEC-${capecObject['_attributes']['ID']}`
+    }];
+}
+
+function genSeverity() {
+    let severity = null;
+
+    const severityObject = capecObject['capec:Typical_Severity'];
+    if (severityObject) {
+        severity = severityObject['_text'];
+    }
+
+    return severity;
+}
+
+function genLikelihood() {
+    let likelihood = null;
+
+    const likelihoodObject = capecObject['capec:Typical_Likelihood_of_Exploit'];
+    if (likelihoodObject && likelihoodObject['capec:Likelihood']) {
+        likelihood = likelihoodObject['capec:Likelihood']['_text'];
+    }
+
+    return likelihood;
+}
+
+function genInjectionVector() {
+    let injectionVector = null;
+
+    const injectionVectorObject = capecObject['capec:Injection_Vector'];
+    if (injectionVectorObject && injectionVectorObject['capec:Text']) {
+        injectionVector = injectionVectorObject['capec:Text']['_text'];
+    }
+
+    return injectionVector;
+}
+
+function genPayload() {
+    let payload = null;
+
+    const payloadObject = capecObject['capec:Payload'];
+    if (payloadObject && payloadObject['capec:Text']) {
+        payload = payloadObject['capec:Text']['_text'];
+    }
+
+    return payload;
+}
+
+function genActivationZone() {
+    let activationZone = null;
+
+    const activationZoneObject = capecObject['capec:Activation_Zone'];
+    if (activationZoneObject && activationZoneObject['capec:Text']) {
+        activationZone = activationZoneObject['capec:Text']['_text'];
+    }
+
+    return activationZone;
+}
+
+function genCia() {
+    let cia = null;
+
+    const ciaObject = capecObject['capec:CIA_Impact'];
+    if (ciaObject && ciaObject !== {}) {
+        cia = {
+            confidentiality: ciaObject['capec:Confidentiality_Impact']['_text'],
+            integrity: ciaObject['capec:Integrity_Impact']['_text'],
+            availability: ciaObject['capec:Availability_Impact']['_text']
+        };
+    }
+
+    return cia;
+}
+
+module.exports = {
+    feed,
+    clear,
+    createEntity,
+    genName,
+    genDescription,
+    genExternalReferences,
+    genSeverity,
+    genLikelihood,
+    genInjectionVector,
+    genPayload,
+    genActivationZone,
+    genCia
+};
---
+++ app/services/stix/general-generator.js
@@ -0,0 +1,44 @@
+/**
+ * Due to CAPEC's complex description structure, this is not 100 % robust, and may not preserve order as it should.
+ *
+ * @param description The description array to be populated.
+ * @param element The element to be recursively parsed for data.
+ * @returns {Array, string} The description array populated with all recursively parsed members.
+ */
+function buildRecursiveCapecDescription(description, element) {
+    if (typeof element === 'string') {
+        return element;
+    } else if (element['_text']) {
+        return description.concat(buildRecursiveCapecDescription(description, element['_text']));
+    } else if (element['capec:Text'] instanceof Array) {
+        return description.concat(...element['capec:Text'].map((c) => {
+            return description.concat(buildRecursiveCapecDescription(description, c));
+        }));
+    } else if (element['capec:Text']) {
+        return description.concat(buildRecursiveCapecDescription(description, element['capec:Text']));
+    } else if (element['capec:Code'] instanceof Array) {
+        return description.concat(...element['capec:Code'].map((c) => {
+            return description.concat(buildRecursiveCapecDescription(description, c));
+        }));
+    } else if (element['capec:Code']) {
+        return description.concat(buildRecursiveCapecDescription(description, element['capec:Code']));
+    } else if (element['capec:Block'] && Object.keys(element['capec:Block']).length > 1) {
+        return description.concat(...Object.keys(element['capec:Block']).map((c) => {
+            return description.concat(buildRecursiveCapecDescription(description, { [c]: element['capec:Block'][c] }));
+        }));
+    } else if (element['capec:Block']) {
+        return description.concat(buildRecursiveCapecDescription(description, element['capec:Block']));
+    } else if (element['capec:Summary'] && Object.keys(element['capec:Summary']).length > 1) {
+        return description.concat(...Object.keys(element['capec:Summary']).map((c) => {
+            return description.concat(buildRecursiveCapecDescription(description, { [c]: element['capec:Summary'][c] }));
+        }));
+    } else if (element['capec:Summary']) {
+        return description.concat(buildRecursiveCapecDescription(description, element['capec:Summary']));
+    }
+
+    return description;
+}
+
+module.exports = {
+    buildRecursiveCapecDescription
+};
