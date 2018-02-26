2e2c0c8279e779deb51b7bb0bdc404bd1a5d87a4
Dag Erik <dagerikhl@gmail.com>
2018-02-26 16:55:19 +0100

    Impl. gen of targets relationships
    This concludes the back-end server functionality.

--- app/services/stix/relationship-generator.js
+++ app/services/stix/relationship-generator.js
@@ -15,8 +15,42 @@ function genMitigationRelationships(attackPattern, courseOfActions) {
     return relationships;
 }
 
-// FIXME
-function genTargetRelationships(attackPattern, weaknesses, objects) {
+function genTargetRelationships(attackPattern, allWeaknesses, objects) {
+    const capecObject = objects.capecObjectsFiltered.filter((e) => {
+        return e['_attributes']['ID'] === attackPattern.external_references[0].id.split('-')[1];
+    })[0];
+
+    let relatedWeaknesses = capecObject['capec:Related_Weaknesses']['capec:Related_Weakness'];
+
+    // Make into array if not one to treat as general case after
+    if (!(relatedWeaknesses instanceof Array)) {
+        relatedWeaknesses = [relatedWeaknesses];
+    }
+
+    const relationships = [];
+    for (let relatedWeakness of relatedWeaknesses) {
+        let weakness = allWeaknesses.filter((e) => {
+            // console.log(relatedWeakness);
+            return e.external_references[0].id.split('-')[1] === relatedWeakness['capec:CWE_ID']['_text'];
+        })[0];
+
+        // Abort if no weakness was matched for the relationship
+        if (!weakness) {
+            break;
+        }
+
+        let relationship = stixGeneralGen.createEntity('relationship');
+        relationship.relationship_type = 'targets';
+        relationship.source_ref = attackPattern.id;
+        relationship.target_ref = weakness.id;
+        relationship.custom = {
+            target_mode: relatedWeakness['capec:Weakness_Relationship_Type']['_text'].toLowerCase()
+        };
+
+        relationships.push(relationship);
+    }
+
+    return relationships;
 }
 
 module.exports = {
