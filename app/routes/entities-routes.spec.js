d782907eadef7e8b9c6c569c432bc827a391c2b4
Dag Erik <dagerikhl@gmail.com>
2018-02-27 16:03:25 +0100

    Finish tests for entities routes

--- app/routes/entities-routes.spec.js
+++ app/routes/entities-routes.spec.js
@@ -5,36 +5,45 @@ chai.should();
 
 describe('The entities routes module', function () {
     // Arrange
-    const appSpy = {
+    const app = {
         get: sinon.spy()
     };
 
-    require('./entities-routes')(appSpy);
+    const req = {
+        method: null,
+        headers: {
+            host: null
+        },
+        url: null
+    };
+    const res = {
+        json: sinon.spy()
+    };
+
+    const emptyResponseString = '{}';
+    const emptyResponseObject = JSON.parse(emptyResponseString);
+
+    require('./entities-routes')(app);
 
     it('should export the /entities route', function () {
         // Assert
-        appSpy.get.calledWith('/entities').should.be.true;
+        app.get.calledWith('/entities').should.be.true;
     });
 
     it('should log a serving statement when requested', function () {
-        // Arrange
-        const req = {
-            method: null,
-            headers: {
-                host: null
-            },
-            url: null
-        };
-        const res = {
-            json: () => {
-            }
-        };
-
         // Act
-        appSpy.get.callArgWith(1, req, res);
+        app.get.callArgWith(1, req, res);
 
         // Assert
         logger.info.calledWithMatch(/Serving.*/).should.be.true;
+    });
+
+    it('should serve JSON response', function () {
+        // Act
+        app.get.callArgWith(1, req, res);
+
+        // Assert
+        res.json.lastCall.args[0].should.be.eql(emptyResponseObject);
     });
 
 });
--- test/test-setup.spec.js
+++ test/test-setup.spec.js
@@ -1,5 +1,7 @@
 const sinon = require('sinon');
 
+const fileHandler = require('../app/services/file-handler');
+
 require('../logger');
 
 beforeEach(function () {
@@ -11,6 +13,9 @@ beforeEach(function () {
     this.sandbox.spy(logger, 'verbose');
     this.sandbox.spy(logger, 'debug');
     this.sandbox.spy(logger, 'silly');
+
+    this.sandbox.stub(fileHandler, 'getFileContent').returns('{}');
+    this.sandbox.stub(fileHandler, 'setFileContent');
 });
 
 afterEach(function () {
