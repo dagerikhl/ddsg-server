e3396d831b9254c24722646a31f6b49f01cc46fb
Dag Erik <dagerikhl@gmail.com>
2018-02-27 17:06:38 +0100

    Update winston to 3.0.0 RC and fix logging

--- logger.js
+++ logger.js
@@ -1,23 +1,37 @@
 const winston = require('winston');
 
-const timeFormat = () => (new Date()).toLocaleTimeString();
+const format = winston.format;
 
-const logger = new (winston.Logger)({
+const myFormat = format.printf((info) => {
+    const timestamp = (new Date()).toLocaleTimeString();
+
+    let message;
+    if (typeof info.message !== 'string' && Object.keys(info.message).length > 1) {
+        message = `\n${JSON.stringify({ ...info.message }, null, 2)}`;
+    } else if (typeof info.message !== 'string') {
+        message = `${JSON.stringify(info.message)}`;
+    } else {
+        message = info.message;
+    }
+
+    return `${timestamp} ${info.level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: format.combine(format.colorize(), myFormat),
     transports: [
-        new (winston.transports.Console)({
-            timestamp: timeFormat,
-            colorize: true
+        new winston.transports.Console({
+            level: 'silly'
+        }),
+        new winston.transports.File({
+            filename: 'error.log',
+            level: 'error'
+        }),
+        new winston.transports.File({
+            filename: 'combined.log'
         })
     ]
 });
-
-// Set log level according to environment
-if (process.env.NODE_ENV === 'production') {
-    logger.level = 'info';
-} else if (process.env.NODE_ENV === 'development') {
-    logger.level = 'debug';
-} else if (process.env.NODE_ENV === 'test') {
-    logger.level = 'debug';
-}
 
 global.logger = logger;
