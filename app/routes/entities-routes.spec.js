a011d590915ba8b08b19d8499a394a23ccbe77fb
Dag Erik <dagerikhl@gmail.com>
2018-05-24 14:27:44 +0200

    Test with and without file system in entities route

--- app/routes/entities-routes.spec.js
+++ app/routes/entities-routes.spec.js
@@ -8,12 +8,7 @@ describe('The entities routes function module', function () {
     let req;
     let res;
 
-    const fileHandler = require('../services/file-handler');
-
     before(function () {
-        sinon.stub(fileHandler, 'getFileContent').returns('{}');
-        sinon.stub(fileHandler, 'setFileContent');
-
         global.entities = {};
 
         app = { get: null };
@@ -28,11 +23,6 @@ describe('The entities routes function module', function () {
         res = { json: null };
     });
 
-    after(function () {
-        fileHandler.getFileContent.restore();
-        fileHandler.setFileContent.restore();
-    });
-
     beforeEach(function () {
         app.get = sinon.spy();
         res.json = sinon.spy();
@@ -40,26 +30,72 @@ describe('The entities routes function module', function () {
         require('./entities-routes')(app);
     });
 
-    it('should export exactly one route', function () {
-        app.get.calledOnce.should.be.true;
+    describe('With file system', function () {
+        const fileHandler = require('../services/file-handler');
+
+        before(function () {
+            process.env.USE_FILE_SYSTEM = 'true';
+
+            sinon.stub(fileHandler, 'getFileContent').returns('{}');
+            sinon.stub(fileHandler, 'setFileContent');
+        });
+
+        after(function () {
+            fileHandler.getFileContent.restore();
+            fileHandler.setFileContent.restore();
+        });
+
+        it('should export exactly one route', function () {
+            app.get.calledOnce.should.be.true;
+        });
+
+        it('should export the /entities route', function () {
+            app.get.calledWith('/entities').should.be.true;
+        });
+
+        it('should log a serving statement when requested', function () {
+            app.get.callArgWith(1, req, res);
+
+            logger.info.calledWithMatch(/Serving.*/).should.be.true;
+        });
+
+        it('should serve JSON response when requested', function () {
+            const emptyResponseString = '{}';
+            const emptyResponseObject = JSON.parse(emptyResponseString);
+
+            app.get.callArgWith(1, req, res);
+
+            res.json.lastCall.args[0].should.be.eql(emptyResponseObject);
+        });
     });
 
-    it('should export the /entities route', function () {
-        app.get.calledWith('/entities').should.be.true;
+    describe('Without file system', function () {
+        before(function () {
+            process.env.USE_FILE_SYSTEM = 'false';
+        });
+
+        it('should export exactly one route', function () {
+            app.get.calledOnce.should.be.true;
+        });
+
+        it('should export the /entities route', function () {
+            app.get.calledWith('/entities').should.be.true;
+        });
+
+        it('should log a serving statement when requested', function () {
+            app.get.callArgWith(1, req, res);
+
+            logger.info.calledWithMatch(/Serving.*/).should.be.true;
+        });
+
+        it('should serve JSON response when requested', function () {
+            const emptyResponseString = '{}';
+            const emptyResponseObject = JSON.parse(emptyResponseString);
+
+            app.get.callArgWith(1, req, res);
+
+            res.json.lastCall.args[0].should.be.eql(emptyResponseObject);
+        });
     });
 
-    it('should log a serving statement when requested', function () {
-        app.get.callArgWith(1, req, res);
-
-        logger.info.calledWithMatch(/Serving.*/).should.be.true;
-    });
-
-    it('should serve JSON response when requested', function () {
-        const emptyResponseString = '{}';
-        const emptyResponseObject = JSON.parse(emptyResponseString);
-
-        app.get.callArgWith(1, req, res);
-
-        res.json.lastCall.args[0].should.be.eql(emptyResponseObject);
-    });
 });
