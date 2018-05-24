47bc56c39d2618f1a05f94190d7e4e6b101debf5
Dag Erik <dagerikhl@gmail.com>
2018-05-24 14:36:32 +0200

    Add tests for with and without file system for update pipe

--- app/services/update-pipe/update-pipe.spec.js
+++ app/services/update-pipe/update-pipe.spec.js
@@ -8,28 +8,60 @@ describe('The update pipe module', function () {
     describe('#fetchUpdatedDataFromSources()', function () {
         let updatePipe;
 
-        before(function () {
-            process.env.USE_FILE_SYSTEM = 'false';
-            process.env.LOCAL_JSON_STORE = 'false';
-        });
-
         beforeEach(function () {
             updatePipe = require('./update-pipe');
         });
 
-        it('should not have any entities in a global variabled before fetching and filtering', function () {
-            expect(entities).to.satisfy((data) => !data || Object.keys(data).length === 0);
+        describe('With file system', function () {
+            let setFileContentSpy;
+
+            const fileHandler = require('../file-handler');
+
+            before(function () {
+                process.env.USE_FILE_SYSTEM = 'true';
+                process.env.LOCAL_JSON_STORE = 'true';
+
+                sinon.stub(fileHandler, 'getFileContent').returns('{}');
+                setFileContentSpy = sinon.spy(fileHandler, 'setFileContent');
+            });
+
+            after(function () {
+                fileHandler.getFileContent.restore();
+                fileHandler.setFileContent.restore();
+            });
+
+            /**
+             * This is really an integration test, it's included to ensure sources are fetched correctly before deploy.
+             */
+            it('should store entities to file after fetching and filtering them', function (done) {
+                updatePipe.fetchUpdatedDataFromSources()
+                    .then(() => {
+                        setFileContentSpy.calledWith('entities.json').should.be.true;
+                    })
+                    .then(done, done);
+            }).timeout(60000);
         });
 
-        /**
-         * This is really an integration test, but is included to ensure sources are fetched correctly before deploy.
-         */
-        it('should store entities into global variable after fetching and filtering them', function (done) {
-            updatePipe.fetchUpdatedDataFromSources()
-                .then(() => {
-                    expect(entities).to.satisfy((data) => data !== null && Object.keys(data).length > 0);
-                })
-                .then(done, done);
-        }).timeout(60000);
+        describe('Without file system', function () {
+            before(function () {
+                process.env.USE_FILE_SYSTEM = 'false';
+                process.env.LOCAL_JSON_STORE = 'false';
+            });
+
+            it('should not have any entities in a global variabled before fetching and filtering', function () {
+                expect(entities).to.satisfy((data) => !data || Object.keys(data).length === 0);
+            });
+
+            /**
+             * This is really an integration test, it's included to ensure sources are fetched correctly before deploy.
+             */
+            it('should store entities into global variable after fetching and filtering them', function (done) {
+                updatePipe.fetchUpdatedDataFromSources()
+                    .then(() => {
+                        expect(entities).to.satisfy((data) => data !== null && Object.keys(data).length > 0);
+                    })
+                    .then(done, done);
+            }).timeout(60000);
+        });
     });
 });
